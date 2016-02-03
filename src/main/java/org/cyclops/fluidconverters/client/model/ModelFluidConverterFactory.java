02ae9d772cb22ea860d5c0895ca52f8f567adb10
immortaleeb <immortaleeb@gmail.com>
2016-02-03 22:55:54 +0100

    Use cyclops' 3rd person view model abstraction
    See CyclopsMC/CyclopsCore@687ca48507b42b4c9674929b4808d428a91b4d31

--- src/main/java/org/cyclops/fluidconverters/client/model/ModelFluidConverterFactory.java
+++ src/main/java/org/cyclops/fluidconverters/client/model/ModelFluidConverterFactory.java
@@ -1,16 +1,11 @@
 package org.cyclops.fluidconverters.client.model;
 
-import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
-import net.minecraftforge.client.model.IFlexibleBakedModel;
-import net.minecraftforge.client.model.IPerspectiveAwareModel;
-import net.minecraftforge.client.model.TRSRTransformation;
 import net.minecraftforge.common.property.IExtendedBlockState;
 import net.minecraftforge.fluids.Fluid;
 import org.cyclops.cyclopscore.client.model.DynamicModel;
@@ -20,7 +15,6 @@ import org.cyclops.fluidconverters.fluidgroup.FluidGroup;
 import org.cyclops.fluidconverters.fluidgroup.FluidGroupReference;
 import org.cyclops.fluidconverters.tileentity.TileFluidConverter;
 
-import javax.vecmath.Vector3f;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -72,28 +66,11 @@ public class ModelFluidConverterFactory extends DynamicModel {
             fluidOutputs = serializer.readPersistedField(TileFluidConverter.NBT_FLUID_OUTPUTS, nbt);
 
             if (fluidGroup != null && fluidOutputs != null) {
-                ModelFluidConverter model = new ModelFluidConverter(this, baseModel, fluidGroup, fluidOutputs);
-                return createPerspectiveAwareModel(model);
+                return new ModelFluidConverter(this, baseModel, fluidGroup, fluidOutputs);
             }
         }
 
         return baseModel;
-    }
-
-    // Third person transform for items
-    private static final TRSRTransformation THIRD_PERSON = TRSRTransformation.blockCenterToCorner(new TRSRTransformation(
-            new Vector3f(0, 1.5f / 16, -2.75f / 16),
-            TRSRTransformation.quatFromYXZDegrees(new Vector3f(10, -45, 170)),
-            new Vector3f(0.375f, 0.375f, 0.375f),
-            null));
-
-    // All transforms that forge doesn't seem to fix by default (?)
-    private static final ImmutableMap<ItemCameraTransforms.TransformType, TRSRTransformation> PERSPECTIVE_TRANSFORMS =
-            ImmutableMap.of(ItemCameraTransforms.TransformType.THIRD_PERSON, THIRD_PERSON);
-
-    // Creates a perspective aware model that fixes some transforms that aren't handled by default
-    private IPerspectiveAwareModel createPerspectiveAwareModel(IFlexibleBakedModel model) {
-        return new IPerspectiveAwareModel.MapWrapper(model, PERSPECTIVE_TRANSFORMS);
     }
 
     @Override
